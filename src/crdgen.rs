a07e170587259c2cbca50b26d5e19df3826e30d0
Jerry Chen <jerry153fish@gmail.com>
2022-04-24 18:32:45 +1000

    feat: #4 add aws crates

--- src/crdgen.rs
+++ src/crdgen.rs
@@ -2,6 +2,6 @@ use kube::CustomResourceExt;
 fn main() {
     print!(
         "{}",
-        serde_yaml::to_string(&controller::Rcrd::crd()).unwrap()
+        serde_yaml::to_string(&controller::RSecret::crd()).unwrap()
     )
 }
