1e6cbbd257a1287a707c7b8d9130be7963492174
Jerry Chen <jerry153fish@gmail.com>
2022-04-25 12:28:13 +1000

    feat: #5 add controller

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,103 @@
-// use futures::{StreamExt, TryStreamExt};
-use k8s_openapi::api::core::v1::Pod;
+use futures::StreamExt;
 use kube::{
     api::{Api, ListParams, ResourceExt},
+    runtime::controller::{Action, Context, Controller},
     Client,
 };
+use std::sync::Arc;
+
+use tokio::time::Duration;
+
+use controller::RSecret;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Infer the runtime environment and try to create a Kubernetes Client
-    let client = Client::try_default().await?;
+    let client = Client::try_default()
+        .await
+        .expect("Expected a valid KUBECONFIG environment variable.");
 
-    // Read pods in the configured namespace into the typed interface from k8s-openapi
-    let pods: Api<Pod> = Api::default_namespaced(client);
-    for p in pods.list(&ListParams::default()).await? {
-        println!("found pod {}", p.name());
+    // Preparation of resources used by the `kube_runtime::Controller`
+    let rsecrets: Api<RSecret> = Api::all(client.clone());
+    for p in rsecrets.list(&ListParams::default()).await? {
+        println!("found {:?}", p);
     }
+
+    let context: Context<ContextData> = Context::new(ContextData::new(client.clone()));
+
+    Controller::new(rsecrets.clone(), ListParams::default())
+        .run(reconcile, on_error, context)
+        .for_each(|reconciliation_result| async move {
+            match reconciliation_result {
+                Ok(echo_resource) => {
+                    println!("Reconciliation successful. Resource: {:?}", echo_resource);
+                }
+                Err(reconciliation_err) => {
+                    eprintln!("Reconciliation error: {:?}", reconciliation_err)
+                }
+            }
+        })
+        .await;
+
     Ok(())
+}
+
+async fn reconcile(echo: Arc<RSecret>, context: Context<ContextData>) -> Result<Action, Error> {
+    let client: Client = context.get_ref().client.clone(); // The `Client` is shared -> a clone from the reference is obtained
+
+    // The resource of `Echo` kind is required to have a namespace set. However, it is not guaranteed
+    // the resource will have a `namespace` set. Therefore, the `namespace` field on object's metadata
+    // is optional and Rust forces the programmer to check for it's existence first.
+    let namespace: String = match echo.namespace() {
+        None => {
+            // If there is no namespace to deploy to defined, reconciliation ends with an error immediately.
+            return Err(Error::UserInputError(
+                "Expected Echo resource to be namespaced. Can't deploy to an unknown namespace."
+                    .to_owned(),
+            ));
+        }
+        // If namespace is known, proceed. In a more advanced version of the operator, perhaps
+        // the namespace could be checked for existence first.
+        Some(namespace) => namespace,
+    };
+
+    Ok(Action::requeue(Duration::from_secs(10)))
+}
+
+fn on_error(error: &Error, _context: Context<ContextData>) -> Action {
+    eprintln!("Reconciliation error:\n{:?}", error);
+    Action::requeue(Duration::from_secs(5))
+}
+
+/// All errors possible to occur during reconciliation
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// Any error originating from the `kube-rs` crate
+    #[error("Kubernetes reported error: {source}")]
+    KubeError {
+        #[from]
+        source: kube::Error,
+    },
+    /// Error in user input or RSecret resource definition, typically missing fields.
+    #[error("Invalid RSecret CRD: {0}")]
+    UserInputError(String),
+}
+
+/// Context injected with each `reconcile` and `on_error` method invocation.
+struct ContextData {
+    /// Kubernetes client to make Kubernetes API requests with. Required for K8S resource management.
+    client: Client,
+}
+
+impl ContextData {
+    /// Constructs a new instance of ContextData.
+    ///
+    /// # Arguments:
+    /// - `client`: A Kubernetes client to make Kubernetes REST API requests with. Resources
+    /// will be created and deleted with this client.
+    pub fn new(client: Client) -> Self {
+        ContextData { client }
+    }
 }
 
 #[cfg(test)]
