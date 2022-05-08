4f9ed584dfe7aac27268aeab81c86021905890e7
Jerry Chen <jerry153fish@gmail.com>
2022-05-08 19:44:07 +1000

    try catch the skd config exception

--- src/aws.rs
+++ src/aws.rs
@@ -8,7 +8,8 @@ use k8s_openapi::ByteString;
 
 use crate::{utils, Backend};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use std::panic;
 
 /// if using localstack as aws backend
 pub fn is_test_env() -> bool {
@@ -60,6 +61,40 @@ pub fn appconfig_client(conf: &aws_types::SdkConfig) -> aws_sdk_appconfig::Clien
     aws_sdk_appconfig::Client::from_conf(appconfig_config_builder.build())
 }
 
+pub async fn catch_sdk_config_panic() -> Result<aws_types::SdkConfig> {
+    let my_complex_type = 1;
+
+    // Wrap it all in a std::sync::Mutex
+    let mutex = std::sync::Mutex::new(my_complex_type);
+    let result = panic::catch_unwind(|| {
+        let my_complex_type = mutex.lock().unwrap();
+
+        // Enter the runtime
+        let handle = tokio::runtime::Handle::current();
+
+        handle.enter();
+
+        futures::executor::block_on(get_aws_sdk_config(*my_complex_type))
+    });
+
+    match result {
+        Ok(result) => match result {
+            Ok(result) => Ok(result),
+            Err(err) => {
+                log::error!("{:?}", err);
+                Err(anyhow!("Failed to get the credentials"))
+            }
+        },
+        Err(err) => {
+            log::error!("{:?}", err);
+            Err(anyhow!("Failed to get the credentials"))
+        }
+    }
+}
+
+pub async fn get_aws_sdk_config(_t: i32) -> Result<aws_types::SdkConfig> {
+    Ok(aws_config::from_env().load().await)
+}
 /// get the data from the ssm parameter store by name
 /// Will cache the result for 60s
 #[cached(time = 60, result = true)]
@@ -68,7 +103,7 @@ pub async fn get_appconfig_configuration_by_version(
     configuration_profile_id: String,
     version_number: i32,
 ) -> Result<String> {
-    let shared_config = aws_config::from_env().load().await;
+    let shared_config = catch_sdk_config_panic().await?;
     let client = appconfig_client(&shared_config);
     let result = client
         .get_hosted_configuration_version()
@@ -91,7 +126,7 @@ pub async fn get_appconfig_configuration_by_version(
 /// Will cache the result for 60s
 #[cached(time = 60, result = true)]
 pub async fn get_ssm_parameter(name: String) -> Result<String> {
-    let shared_config = aws_config::from_env().load().await;
+    let shared_config = catch_sdk_config_panic().await?;
     let client = ssm_client(&shared_config);
     let parmeter = client.get_parameter().name(name).send().await?;
     let result = parmeter.parameter().unwrap().value().unwrap_or_default();
