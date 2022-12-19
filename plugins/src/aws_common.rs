26757440ffe6f23ebef89527b5acb75128dd039a
Jerry Chen <jerry153fish@gmail.com>
2022-12-19 16:03:01 +1100

    chore: bump up aws packages versions

--- plugins/src/aws_common.rs
+++ plugins/src/aws_common.rs
@@ -1,7 +1,5 @@
 use anyhow::Result;
 use aws_smithy_http::endpoint::Endpoint;
-use http::Uri;
-use std::str::FromStr;
 
 /// if using localstack as aws backend
 pub fn is_test_env() -> bool {
@@ -12,7 +10,7 @@ pub fn is_test_env() -> bool {
 pub fn localstack_endpoint() -> Endpoint {
     let local_endpoint = "http://localhost:4566/".to_string();
     let url = std::env::var("LOCALSTACK_URL").unwrap_or(local_endpoint);
-    Endpoint::immutable(Uri::from_str(&url).unwrap())
+    Endpoint::immutable(url).unwrap()
 }
 
 pub async fn get_aws_sdk_config() -> Result<aws_types::SdkConfig> {
